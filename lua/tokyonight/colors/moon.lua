6e58e71165bf0248a1cde246ce93999b4dc1054d
William <william.seipp@gmail.com>
2025-05-01 16:17:37 -0700

    change lime green to subdued yellow

--- lua/tokyonight/colors/moon.lua
+++ lua/tokyonight/colors/moon.lua
@@ -5,36 +5,34 @@ return {
   bg_dark1 = "#191B29",
   bg_highlight = "#2b303c",
   blue0 = "#4f596e",
+  fg_gutter = "#4a5468", -- line numbers
+  comment = "#4a5468", -- comments
+
   blue1 = "#c3c6cd", -- telescope border
+
   blue2 = "#e2b714", -- rubocop error and [!note] for meander callouts
-  blue5 = "#f6f8fb", -- commas, curly brace
+  green1 = "#e6de98", --
   blue6 = "#e6de98",
   dark3 = "#f0c674", -- numbers for git telescope
+  orange = "#f0c674", -- markdown headings
 
+  green = "#5f819d", -- strings
+  green2 = "#41a6b5",
   magenta = "#88c0d0",
   cyan = "#88c0d0", -- keywords1
   purple = "#c0c7d3", -- keywords2
-  yellow = "#b3bdd5", -- variables and parameters
-  blue = "#b3bdd5", -- function names
+  yellow = "#c0c7d3", -- variables and parameters
+  blue = "#c0c7d3", -- function names
+  fg = "#c0c7d3",
+  fg_dark = "#c0c7d3", -- filenames in nerdtree
 
-  comment = "#4a5468", -- comments
-
+  blue5 = "#f6f8fb", -- commas, curly brace
   dark5 = "#ffffff", -- squares in auto fill
-  fg = "#b2b3b6",
-  --fg = "#aeb2bb",
-  --fg_dark = "#828bb8",
-  fg_dark = "#c3c6cd", -- filenames in nerdtree
-  --fg_gutter = "#3b4261",
-  fg_gutter = "#4a5468", -- line numbers
-  -- green = "#c3e88d",
-  green = "#5f819d", -- strings
-  green1 = "#ABE946", --
-  green2 = "#41a6b5",
-  magenta2 = "#ff005f", -- highlight for LEAP
-  orange = "#f0c674", -- markdown headings
-  red = "#ea6060", -- KEYWORD SELF in RUBY, deleted lines in lazygit
-  red1 = "#db4b4b",
   teal = "#f5f2d6", -- warnings from linter
+
+  magenta2 = "#ff005f", -- highlight for LEAP
+  red1 = "#db4b4b",
+  red = "#ea6060", -- KEYWORD SELF in RUBY, deleted lines in lazygit
   terminal_black = "#cc7777",
   blue7 = "#cc7777",
   git = {
