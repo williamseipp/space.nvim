92bfa17d1ef762d08ee8fb453ad51d4dda3efca2
William <william.seipp@gmail.com>
2025-06-08 09:57:06 -0700

    try out perplexity UI

--- lua/tokyonight/colors/moon.lua
+++ lua/tokyonight/colors/moon.lua
@@ -1,7 +1,7 @@
 ---@type Palette
 return {
-  bg = "#262b35",
-  bg_dark = "#262b35",
+  bg = "#202222",
+  bg_dark = "#202222",
   bg_dark1 = "#191B29",
   bg_highlight = "#2b303c",
   blue0 = "#4f596e",
@@ -10,27 +10,27 @@ return {
 
   dark5 = "#f1ebdd", -- squares in auto fill
   teal = "#f1ebdd", -- warnings from linter
-  fg = "#bfbfbf",
-  blue1 = "#bfbfbf", -- telescope border
-  green1 = "#bfbfbf", --
-  yellow = "#bfbfbf", -- variables and parameters
-  blue = "#88c0d0", -- function names
-  red = "#bfbfbf", -- KEYWORD SELF in RUBY, deleted lines in lazygit
-  red1 = "#bfbfbf",
-  fg_dark = "#bfbfbf", -- filenames in nerdtree
+  fg = "#8d9191",
+  blue1 = "#8d9191", -- telescope border
+  green1 = "#8d9191", --
+  yellow = "#8d9191", -- variables and parameters
+  blue = "#e6dc90", -- function names
+  red = "#8d9191", -- KEYWORD SELF in RUBY, deleted lines in lazygit
+  red1 = "#8d9191",
+  fg_dark = "#8d9191", -- filenames in nerdtree
   blue5 = "#b0b4ba", -- commas, curly brace
 
   blue2 = "#e2b714", -- rubocop error and [!note] for meander callouts
 
-  blue6 = "#c0bfaa",
-  dark3 = "#c0bfaa", -- numbers for git telescope
-  orange = "#c0bfaa", -- markdown headings
-  green = "#c0bfaa", -- strings
+  blue6 = "#8fbd77",
+  dark3 = "#8fbd77", -- numbers for git telescope
+  orange = "#8fbd77", -- markdown headings
+  green = "#8fbd77", -- strings
 
   green2 = "#41a6b5",
-  magenta = "#bfbfbf",
-  cyan = "#bfbfbf", -- keywords1
-  purple = "#bfbfbf", -- keywords2
+  magenta = "#8d9191",
+  cyan = "#8d9191", -- keywords1
+  purple = "#8d9191", -- keywords2
 
   magenta2 = "#ff005f", -- highlight for LEAP
   terminal_black = "#cc7777",
