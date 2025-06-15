28b5a709b756141b6f7a6dacea90211aebbb4f23
William <william.seipp@gmail.com>
2025-06-15 04:44:52 -0700

    change back

--- lua/tokyonight/colors/moon.lua
+++ lua/tokyonight/colors/moon.lua
@@ -1,7 +1,7 @@
 ---@type Palette
 return {
-  bg = "#23514d",
-  bg_dark = "#23514d",
+  bg = "#262b35",
+  bg_dark = "#262b35",
   bg_dark1 = "#191B29",
   bg_highlight = "#2b303c",
   blue0 = "#4f596e",
@@ -10,26 +10,26 @@ return {
 
   dark5 = "#f1ebdd", -- squares in auto fill
   teal = "#f1ebdd", -- warnings from linter
-  fg = "#619d8b",
-  blue1 = "#619d8b", -- telescope border
-  green1 = "#619d8b", --
-  yellow = "#619d8b", -- variables and parameters
-  blue = "#619d8b", -- function names
-  red = "#5dc997", -- KEYWORD SELF in RUBY, deleted lines in lazygit
-  red1 = "#619d8b",
-  fg_dark = "#619d8b", -- filenames in nerdtree
-  blue5 = "#619d8b", -- commas, curly brace
+  fg = "#bfbfbf",
+  blue1 = "#88c0d0", -- telescope border
+  green1 = "#bfbfbf", --
+  yellow = "#bfbfbf", -- variables and parameters
+  blue = "#bfbfbf", -- function names
+  red = "#88c0d0", -- KEYWORD SELF in RUBY, deleted lines in lazygit
+  red1 = "#bfbfbf",
+  fg_dark = "#bfbfbf", -- filenames in nerdtree
+  blue5 = "#b0b4ba", -- commas, curly brace
 
   blue2 = "#e4d084", -- rubocop error and [!note] for meander callouts
-  blue6 = "#a6c1c3",
-  dark3 = "#a6c1c3", -- numbers for git telescope
-  orange = "#a6c1c3", -- markdown headings
-  green = "#dce3e1", -- strings
+  blue6 = "#c0bfaa",
+  dark3 = "#c0bfaa", -- numbers for git telescope
+  orange = "#c0bfaa", -- markdown headings
+  green = "#c0bfaa", -- strings
 
   green2 = "#41a6b5",
-  magenta = "#5dc997",
-  cyan = "#5dc997", -- keywords1
-  purple = "#5dc997", -- keywords2
+  magenta = "#58dda9",
+  cyan = "#7bc1b0", -- keywords1
+  purple = "#88c0d0", -- keywords2
 
   magenta2 = "#ff005f", -- highlight for LEAP
   terminal_black = "#cc7777",
