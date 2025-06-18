a9010bcde2301206b00c7d4b34ce0fa95fde1461
William <william.seipp@gmail.com>
2025-06-18 13:56:27 -0700

    organize colors

--- lua/tokyonight/colors/moon.lua
+++ lua/tokyonight/colors/moon.lua
@@ -7,36 +7,31 @@ return {
   blue0 = "#4f596e",
   fg_gutter = "#4a5468", -- line numbers
   comment = "#e4d084", -- comments
+  blue2 = "#e4d084", -- rubocop error and [!note] for meander callouts
 
-  -- #a798bf
-  -- #9688ab
-  -- #786d89
-  -- #4f4461
   --dark5 = "#e7d0b4", -- squares in auto fill
   --teal = "#f1ebdd", -- warnings from linter
   dark5 = "#f2f6fb", -- squares in auto fill
   teal = "#f2f6fb", -- warnings from linter
   fg = "#d6dae0",
-  blue1 = "#88c0d0", -- telescope border
   green1 = "#d6dae0", --
   yellow = "#d6dae0", -- variables and parameters
   blue = "#d6dae0", -- function names
-  red = "#88c0d0", -- KEYWORD SELF in RUBY, deleted lines in lazygit
   red1 = "#d6dae0",
   fg_dark = "#d6dae0", -- filenames in nerdtree
+  orange = "#d6dae0", -- markdown headings
   blue5 = "#c5c8c6", -- commas, curly brace
 
-  blue2 = "#e4d084", -- rubocop error and [!note] for meander callouts
+  green = "#a3bd87", -- strings
+  cyan = "#7bc1b0", -- keywords1
+  green2 = "#41a6b5",
+  blue1 = "#88c0d0", -- telescope border
+  red = "#88c0d0", -- KEYWORD SELF in RUBY, deleted lines in lazygit
+  magenta = "#88c0d0", -- keywords0
+  purple = "#88c0d0", -- keywords2
+
   blue6 = "#cc7777",
   dark3 = "#cc7777", -- numbers for git telescope
-  -- orange = "#a798bf", -- markdown headings
-  orange = "#d6dae0", -- markdown headings
-  green = "#a3bd87", -- strings
-
-  green2 = "#41a6b5",
-  magenta = "#88c0d0", -- keywords0
-  cyan = "#7bc1b0", -- keywords1
-  purple = "#88c0d0", -- keywords2
 
   magenta2 = "#ff005f", -- highlight for LEAP
   terminal_black = "#cc7777",
