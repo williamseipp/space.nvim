9543cd5df1e521990a1f6bb698cdf2f91f8742ee
William <william.seipp@gmail.com>
2025-07-03 15:43:51 -0700

    so lazy

--- lua/tokyonight/colors/moon.lua
+++ lua/tokyonight/colors/moon.lua
@@ -20,7 +20,7 @@ return {
   orange = "#d2d3d4", -- markdown headings
   blue5 = "#d2d3d4", -- commas, curly brace
 
-  green = "#368468", -- strings
+  green = "#8abeb7", -- strings
   blue1 = "#d2d3d4", -- telescope border, some keywords?
   green2 = "#41a6b5",
   cyan = "#cc7777", -- keywords1
