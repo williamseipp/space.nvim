97c08a6bf4f4c3f7a009c5d7f38e82694d1e2f6e
William <william.seipp@gmail.com>
2025-07-03 15:46:03 -0700

    so lazy

--- lua/tokyonight/colors/moon.lua
+++ lua/tokyonight/colors/moon.lua
@@ -2,12 +2,12 @@
 return {
   bg = "#292d33",
   bg_dark = "#292d33",
-  bg_dark1 = "#e4d084",
   bg_highlight = "#4a5468",
   blue0 = "#4a5468",
   fg_gutter = "#4a5468", -- line numbers
   comment = "#4a5468", -- comments
   blue2 = "#e4d084", -- rubocop error and [!note] for meander callouts
+  bg_dark1 = "#e4d084",
 
   dark5 = "#f2f6fb", -- squares in auto fill
   teal = "#f2f6fb", -- warnings from linter
@@ -19,21 +19,21 @@ return {
   fg_dark = "#d2d3d4", -- filenames in nerdtree
   orange = "#d2d3d4", -- markdown headings
   blue5 = "#d2d3d4", -- commas, curly brace
+  red = "#d2d3d4", -- KEYWORD SELF in RUBY, deleted lines in lazygit
+  blue1 = "#d2d3d4", -- telescope border, some keywords?
 
   green = "#8abeb7", -- strings
-  blue1 = "#d2d3d4", -- telescope border, some keywords?
   green2 = "#41a6b5",
-  cyan = "#cc7777", -- keywords1
-  red = "#d2d3d4", -- KEYWORD SELF in RUBY, deleted lines in lazygit
   magenta = "#81a2be", -- keywords0
   purple = "#81a2be", -- keywords2
 
+  cyan = "#cc7777", -- keywords1
   blue6 = "#cc7777",
   dark3 = "#cc7777", -- numbers for git telescope
-
-  magenta2 = "#8fbd77", -- highlight for LEAP
   terminal_black = "#cc7777",
   blue7 = "#cc7777",
+
+  magenta2 = "#8fbd77", -- highlight for LEAP
   git = {
     add = "#8fbd77",
     change = "#e4d084",
