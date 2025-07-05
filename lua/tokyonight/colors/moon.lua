691fb4142ae55e2cf355d11d2bc89916d93cfedf
William <william.seipp@gmail.com>
2025-07-05 10:40:50 -0700

    so lazy

--- lua/tokyonight/colors/moon.lua
+++ lua/tokyonight/colors/moon.lua
@@ -5,7 +5,7 @@ return {
   bg_highlight = "#4a5468",
   blue0 = "#4a5468",
   fg_gutter = "#4a5468", -- line numbers
-  comment = "#7383a2", -- comments
+  comment = "#858fa2", -- comments
   blue2 = "#e4d084", -- rubocop error and [!note] for meander callouts
   bg_dark1 = "#e4d084",
 
